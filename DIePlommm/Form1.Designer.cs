27e3fa63f5199b3b23adba33d066309e3f15c4ca
Trigonometry <macdevzenit@gmail.com>
2019-05-31 13:31:15 +0300

    Just Commit

---
+++ DIePlommm/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace DIePlommm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chartMbits = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonResetZoom = new System.Windows.Forms.Button();
+            this.buttonClearGraph = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelIntervalError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartMbits)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(228, 108);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.textBox1.Location = new System.Drawing.Point(888, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // chartMbits
+            // 
+            this.chartMbits.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chartMbits.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartMbits.Legends.Add(legend1);
+            this.chartMbits.Location = new System.Drawing.Point(242, 55);
+            this.chartMbits.Name = "chartMbits";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Входящий трафик";
+            this.chartMbits.Series.Add(series1);
+            this.chartMbits.Size = new System.Drawing.Size(755, 260);
+            this.chartMbits.TabIndex = 5;
+            this.chartMbits.Text = "chart2";
+            // 
+            // buttonResetZoom
+            // 
+            this.buttonResetZoom.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttonResetZoom.Location = new System.Drawing.Point(0, 199);
+            this.buttonResetZoom.Name = "buttonResetZoom";
+            this.buttonResetZoom.Size = new System.Drawing.Size(228, 55);
+            this.buttonResetZoom.TabIndex = 6;
+            this.buttonResetZoom.Text = "Сбросить приближение";
+            this.buttonResetZoom.UseVisualStyleBackColor = false;
+            this.buttonResetZoom.Click += new System.EventHandler(this.ButtonResetZoom_Click);
+            // 
+            // buttonClearGraph
+            // 
+            this.buttonClearGraph.Location = new System.Drawing.Point(0, 260);
+            this.buttonClearGraph.Name = "buttonClearGraph";
+            this.buttonClearGraph.Size = new System.Drawing.Size(228, 55);
+            this.buttonClearGraph.TabIndex = 7;
+            this.buttonClearGraph.Text = "Очистить график";
+            this.buttonClearGraph.UseVisualStyleBackColor = true;
+            this.buttonClearGraph.Click += new System.EventHandler(this.ButtonClearGraph_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Статус";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Status";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(132, 116);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(45, 20);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Интервал в секундах";
+            // 
+            // labelIntervalError
+            // 
+            this.labelIntervalError.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelIntervalError.Location = new System.Drawing.Point(12, 139);
+            this.labelIntervalError.Name = "labelIntervalError";
+            this.labelIntervalError.Size = new System.Drawing.Size(216, 57);
+            this.labelIntervalError.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1000, 374);
+            this.Controls.Add(this.labelIntervalError);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonClearGraph);
+            this.Controls.Add(this.buttonResetZoom);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chartMbits);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Оценка интенсивности трафика";
+            ((System.ComponentModel.ISupportInitialize)(this.chartMbits)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartMbits;
+        private System.Windows.Forms.Button buttonResetZoom;
+        private System.Windows.Forms.Button buttonClearGraph;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelIntervalError;
+    }
+}
---
+++ DIePlommm/TrafficIntensityEstimate.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DIePlommm
+{
+    class TrafficIntensityEstimate
+    {
+
+        private PerformanceCounter inputTrafficCounter;
+        private PerformanceCounterCategory[] categoriesOfCounters;
+        private int indexOfNetworkInterface;
+        
+        /**
+         * Конструктор
+         */
+        public TrafficIntensityEstimate()
+        {
+            categoriesOfCounters = PerformanceCounterCategory.GetCategories();
+            indexOfNetworkInterface = findNetworkInterface();
+
+            string[] instanceNames = getNetworkInterfaces();
+            inputTrafficCounter = new PerformanceCounter(categoriesOfCounters[indexOfNetworkInterface].CategoryName, "Bytes Received/sec", instanceNames[0]);
+        }
+
+
+        /**
+         * Ищет категорию сетевых интерфейсов среди всех интерфейсов
+         */
+        private int findNetworkInterface()
+        {
+            for (int i = 0; i < categoriesOfCounters.Length; i++)
+                if (categoriesOfCounters[i].CategoryName.Equals("Network Interface"))
+                    return i;
+            return -1;
+        }
+
+        
+        /**
+         * Возвращает все сетевые интерфейсы которые есть на компьютере
+         */
+        public string[] getNetworkInterfaces()
+        {
+            string[] instanceNames = categoriesOfCounters[indexOfNetworkInterface].GetInstanceNames();
+            return instanceNames;
+        }
+
+
+        /**
+         * Опредеялет сетевой интерфейс для Counter
+         */
+        public void setNetworkInterface(String networkInterface)
+        {
+            inputTrafficCounter = new PerformanceCounter(categoriesOfCounters[indexOfNetworkInterface].CategoryName, "Bytes Received/sec", networkInterface);
+        }
+
+        /**
+         * Возвращает значение Counter
+         */
+        public float getInputTraffic()
+        {
+            return inputTrafficCounter.NextValue();
+        }
+
+
+        /**
+         *На основе того идет ли трафик будет определяться есть подключение или нет
+         */
+        public bool isNetworkConnection()
+        {
+            if(getInputTraffic() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string getStringMBits()
+        {
+            double data = Math.Round(inputTrafficCounter.NextValue(), 3) / 125000;
+            return data.ToString() + "Mbits";
+        }
+
+        public string getStringKBits()
+        {
+            double data = Math.Round(inputTrafficCounter.NextValue(), 3) / 125;
+            return data.ToString() + "Mbits";
+        }
+
+        
+    }
+}
